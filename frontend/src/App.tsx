74739ee0571d56557fba55f650ff45a0c9ecc526
AraceliDev <aracelipmj1992@gmail.com>
2025-09-03 10:23:49 +0200

    fix: AppRouter

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,7 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import Workouts from './pages/workouts/Workouts'
-import Rutinas from './pages/rutinas/Rutinas'
-import Recursos from './pages/recursos/Recursos'
-import Header from './components/layout/Header'
-import Footer from './components/layout/Footer'
+import AppRouter from './router/AppRouter'
 
 function App() {
-  return (
-    <BrowserRouter>
-    <Header /> 
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/workouts" element={<Workouts />} />
-          <Route path="/rutinas" element={<Rutinas />} />
-          <Route path="/recursos" element={<Recursos />} />
-        </Routes>
-    <Footer />  
-    </BrowserRouter>
-  )
+  return <AppRouter />
 }
 
 export default App
--- frontend/src/router/AppRouter.tsx
+++ frontend/src/router/AppRouter.tsx
@@ -1,21 +1,23 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Layout from '../components/layout/Header'
+import Header from '../components/layout/Header'
+import Footer from '../components/layout/Footer'
 import Home from '../pages/Home'
 import Workouts from '../pages/workouts/Workouts'
-import Rutinas from './pages/Rutinas'
-import Recursos from './pages/Recursos'
+import Rutinas from '../pages/rutinas/Rutinas'
+import Recursos from '../pages/recursos/Recursos'
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/workouts" element={<Workouts />} />
-          <Route path="/rutinas" element={<Rutinas />} />
-          <Route path="/recursos" element={<Recursos />} />
-        </Routes>
-      </Layout>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/workouts" element={<Workouts />} />
+        <Route path="/rutinas" element={<Rutinas />} />
+        <Route path="/recursos" element={<Recursos />} />
+      </Routes>
+      <Footer />
     </BrowserRouter>
   )
 }
